3dd3a3d314c1706a83f05edb3c94b30ab3afd815
Thiago-Pereira <tp.maiochi@gmail.com>
2020-11-03 19:30:46 -0300

    Pagina Inicial

---
+++ resources/views/home.blade.php
@@ -0,0 +1,167 @@
+@extends('layouts.index')
+@section('content')
+
+    <div class="container">
+        <div class="show">
+            <a href="">
+                <img src="https://designconceitual.com.br/wp-content/uploads/2016/07/Marvel_Studios_logo.svg_.png"
+                style="width: 500px; margin:0 auto; padding: 10px 10px 10px 10px" alt="">
+            </a>
+        </div>
+        <h3 style="color: white; text-align: center;">O que deseja fazer ?</h3>
+        <div class="row" style="justify-content: center">
+            <ul>
+                <li>
+                    <a href="" style="color: white">Ver Todos os Personagens</a>
+                </li>
+                <li>
+                    <a id="busca" style="color: white">Realizar pesquisa de Personagem</a>
+                </li>
+            </ul>
+
+        </div>
+
+
+
+    </div>
+
+    <div id="search">
+        <div class="container">
+            <div class="row" style="justify-content: center;">
+                <button
+                    class="btn btn-primary col-lg-5 display-7"
+                    data-toggle="collapse"
+                    data-target="#Personagem"
+                    aria-expanded="false"
+                    aria-controls="Personagem"
+                    data-parent="#accordionEx"
+                    style="border-radius: 40px;  margin: .4rem .8rem;"
+                >
+                        Por Personagem
+                </button>
+                <button
+                    class="btn btn-primary col-lg-5 display-7"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#Comics"
+                    aria-expanded="false"
+                    aria-controls="Comics"
+                    data-parent="#accordionEx"
+                    style="border-radius: 40px;  margin: .4rem .8rem;"
+                >
+                        Por Quadrinho
+                </button>
+                <button
+                    class="btn btn-primary col-lg-5 display-7"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#Events"
+                    aria-expanded="false"
+                    aria-controls="Events"
+                    data-parent="#accordionEx"
+                    style="border-radius: 40px;  margin: .4rem .8rem;"
+                >
+                        Por Evento
+                </button>
+                <button
+                    class="btn btn-primary col-lg-5 display-7"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#Serie"
+                    aria-expanded="false"
+                    aria-controls="Serie"
+                    data-parent="#accordionEx"
+                    style="border-radius: 40px;  margin: .4rem .8rem;"
+                >
+                        Por Série
+                </button>
+                <button
+                    class="btn btn-primary col-lg-5 display-7"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#Stories"
+                    aria-expanded="false"
+                    aria-controls="Stories"
+                    data-parent="#accordionEx"
+                    style="border-radius: 40px; margin: .4rem .8rem;"
+                >
+                        Por História
+                </button>
+            </div>
+        </div>
+
+        <div class="container">
+
+            <div class="accordion md-accordion" id="accordionEx" role="tablist" aria-multiselectable="true">
+
+                <div class="row justify-content-center panel-collapse collapse show" data-parent="#accordionEx" id="Personagem">
+                    <form action="" method="post">
+                        @csrf
+                        <div class="input-group" style="background-color: #d40317; flex-direction: unset;">
+                            <input type="text" class="form-control display-4" placeholder="Digite o Personagem">
+                            <span class="input-group-append">
+                                <button class="btn btn-secondary display-4 mbr-fonts-style" type="button" id="btnBuscar"><i class="fas fa-search"></i></button>
+                            </span>
+                        </div>
+                    </form>
+                </div>
+                <div class="row justify-content-center panel-collapse collapse in" data-parent="#accordionEx" id="Comics">
+                    <form action="" method="post">
+                        @csrf
+                        <div class="input-group" style="background-color: #d40317; flex-direction: unset;">
+                            <input type="text" class="form-control display-4" placeholder="Digite o Quadrinho">
+                            <span class="input-group-append">
+                                <button class="btn btn-secondary display-4 mbr-fonts-style" type="button" id="btnBuscar"><i class="fas fa-search"></i></button>
+                            </span>
+                        </div>
+                    </form>
+                </div>
+                <div class="row justify-content-center panel-collapse collapse in" data-parent="#accordionEx" id="Events">
+                    <form action="" method="post">
+                        @csrf
+                        <div class="input-group" style="background-color: #d40317; flex-direction: unset;">
+                            <input type="text" class="form-control display-4" placeholder="Digite o Evento">
+                            <span class="input-group-append">
+                                <button class="btn btn-secondary display-4 mbr-fonts-style" type="button" id="btnBuscar"><i class="fas fa-search"></i></button>
+                            </span>
+                        </div>
+                    </form>
+                </div>
+                <div class="row justify-content-center panel-collapse collapse in" data-parent="#accordionEx" id="Serie">
+                    <form action="" method="post">
+                        @csrf
+                        <div class="input-group" style="background-color: #d40317; flex-direction: unset;">
+                            <input type="text" class="form-control display-4" placeholder="Digite a Série">
+                            <span class="input-group-append">
+                                <button class="btn btn-secondary display-4 mbr-fonts-style" type="button" id="btnBuscar"><i class="fas fa-search"></i></button>
+                            </span>
+                        </div>
+                    </form>
+                </div>
+                <div class="row justify-content-center panel-collapse collapse in" data-parent="#accordionEx" id="Stories">
+                    <form action="" method="post">
+                        @csrf
+                        <div class="input-group" style="background-color: #d40317; flex-direction: unset;">
+                            <input type="text" class="form-control display-4" placeholder="Digite a História">
+                            <span class="input-group-append">
+                                <button class="btn btn-secondary display-4 mbr-fonts-style" type="button" id="btnBuscar"><i class="fas fa-search"></i></button>
+                            </span>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        $(document).ready(function(){
+            $("#search").hide();
+
+            $("#busca").click(function(){
+                $("#search").show()
+            });
+
+        })
+    </script>
+
+@endsection
---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="author" content="Thiago Maiochi">
+    <title>Marvel</title>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+    <link rel="stylesheet" href="{{asset('css/index.css')}}">
+</head>
+<body>
+
+    @yield('content')
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+</body>
+</html>
